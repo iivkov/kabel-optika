b131077b30fcdb42ac25f8abdd5a108bdbb56bb6
Ivan <ivan.ivkovic1996@gmail.com>
2024-01-03 19:49:15 +0100

    podatci o EU projektu GOTOVI

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -7,7 +7,6 @@ const ProjectsComponent = () => {
     
     return (
     <div className="main_content">
-        <img src={Petrinja} alt="Slika Petrinje." style={{ width: '100%', height: 'auto' }} />
         <h1>NOVI RAZVOJ U PETRINJI KABEL-OPTIKA d.o.o.</h1>
         <h2>Kod projekta: KK.08.2.1.16.0148</h2>
         <h2>Naziv poziva: Podrška razvoju poduzetništva u gradu Petrinji</h2>
@@ -19,15 +18,16 @@ const ProjectsComponent = () => {
                     Projekt vodi računa o zaštiti okoliša.</p>
             <div className="box">
                 <h3>Informacije o troškovima</h3>
-                <p>Ukupni prihvatljivi troškovi (EUR): 207.850,55</p>
-                <p>Ukupna bespovratna sredstva (EUR): 176.672,97</p>
+                <p>Ukupna vrijednost projekta (EUR): 263.997,69</p>
+                <p>Ukupni prihvatljivi troškovi (EUR): 209.364,52</p>
+                <p>Ukupna bespovratna sredstva (EUR): 177.959,84</p>
             </div>
             {/* <div className="spacer"></div> */}
             <div className="box">
                 <h3>Vrijeme provedbe</h3>
-                <p>Početak provedbe: 22.04.2022.</p>
-                <p>Kraj provedbe: 19.10.2023.</p>
-                <p>Trajanje provedbe (mjeseci): 18</p>
+                <p>Početak provedbe: 04/2022</p>
+                <p>Kraj provedbe: 12/2023</p>
+                <p>Trajanje provedbe (mjeseci): 20</p>
             </div>
         </div>
         <EUnoteProject />
