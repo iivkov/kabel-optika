3b4781a1d69a08bffa8fafea4f4ce82020d56271
Ivan <ivan.ivkovic1996@gmail.com>
2024-02-17 10:37:44 +0100

    ponovna izmjena na zahtjev, novi build

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -20,8 +20,8 @@ const ProjectsComponent = () => {
         <h1>NOVI RAZVOJ U PETRINJI KABEL-OPTIKA d.o.o.</h1>
         <h2>Kod projekta: KK.08.2.1.16.0148</h2>
         <h2>Naziv poziva: Podrška razvoju poduzetništva u gradu Petrinji</h2>
-        <h2>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</h2>
-        <h2>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</h2>
+        {/* <h2>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</h2>
+        <h2>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</h2> */}
         <div className="container">
             <div className="box">
                 <p>Cilj ovog projekta je održiva fizička, socijalna i gospodarska regeneracija grada Petrinje, s ciljem smanjenja socijalnih nejednakosti, isključenosti i siromaštva. 
@@ -39,7 +39,7 @@ const ProjectsComponent = () => {
                 <h3>Informacije o troškovima</h3>
                 <p>Ukupna vrijednost projekta (EUR): 263.997,69</p>
                 <p>Ukupni prihvatljivi troškovi (EUR): 209.364,52</p>
-                <p>Ukupna bespovratna sredstva (EUR): 177.959,84 (EU sufinanciranje)</p>
+                <p>Ukupna bespovratna sredstva (EUR): 177.959,84</p>
             </div>
             {/* <div className="spacer"></div> */}
             <div className="box">
@@ -49,8 +49,54 @@ const ProjectsComponent = () => {
                 <p>Trajanje provedbe (mjeseci): 20</p>
             </div>
         </div>
+        <EUnoteProject />
         <div className="container">
-            <div className="box">
+            <div className="activity-box">
+                <h3>Aktivnosti projekta</h3>
+                <p>Aktivnost 1. Ulaganje u materijalnu imovinu</p>
+                <p>Aktivnost 2. Ulaganje u materijalnu imovinu</p>
+                <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
+                <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
+                <p>Aktivnost 5: Prijava na Poziv</p>
+                <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
+                <p>Aktivnost 7: Promicanje horizontalnih načela</p>
+                <p>Aktivnost 8: Upravljanje projektom</p>
+                <p>Aktivnost 9: Promidžba i vidljivost</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
+                <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
+                <p>1. planiran je postupak nabave</p>
+                <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
+                <p>3. proveden je postupak nabave</p>
+                <p>4. odabran je isporučitelj roba i/ili usluga</p>
+                <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
+                <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u Ugovoru.</p>
+                <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
+                <p>8. izdan je račun od strane isporučitelja</p>
+                <p>9. račun je plaćen u roku</p>
+                <br />
+                <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
+                <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
+                <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
+                <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+            </div>
+        </div>
+        <EUnoteProject />
+        <div className="container">
+            <div className="activity-box">
+                <h3>Podatci o projektu</h3>
+                <p>Kod projekta: KK.08.2.1.16.0148</p>
+                <p>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</p>
+                <p>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</p>
+                <p>Početak provedbe: 21.04.2022.</p>
+                <p>Kraj provedbe: 12.12.2023.</p>
+                <p>Trajanje provedbe (mjeseci): 20</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
                 <h3>Podatci o korisniku</h3>
                 <p>Prijavitelj: KABEL-OPTIKA društvo s ograničenom odgovornošću za trgovinu i prijevoz</p>
                 <p>Jedinstveni broj prijavitelja: 84074605978</p>
@@ -60,40 +106,11 @@ const ProjectsComponent = () => {
                 <p>Odgovorna osoba: IVAN KREŠIĆ</p>
             </div>
         </div>
-        <EUnoteProject />
         <div className="container">
-            <div className="activity-box">
-                    <h3>Aktivnosti projekta</h3>
-                    <p>Aktivnost 1. Ulaganje u materijalnu imovinu</p>
-                    <p>Aktivnost 2. Ulaganje u materijalnu imovinu</p>
-                    <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
-                    <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
-                    <p>Aktivnost 5: Prijava na Poziv</p>
-                    <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
-                    <p>Aktivnost 7: Promicanje horizontalnih načela</p>
-                    <p>Aktivnost 8: Upravljanje projektom</p>
-                    <p>Aktivnost 9: Promidžba i vidljivost</p>
-            </div>
-        </div>
-        <div className="container">
-            <div className="activity-box">
-                    <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
-                    <p>1. planiran je postupak nabave</p>
-                    <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
-                    <p>3. proveden je postupak nabave</p>
-                    <p>4. odabran je isporučitelj roba i/ili usluga</p>
-                    <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima
-i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
-                    <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u
-Ugovora.</p>
-                    <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
-                    <p>8. izdan je račun od strane isporučitelja</p>
-                    <p>9. račun je plaćen u roku</p>
-                    <br />
-                    <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
-                    <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
-                    <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
-                    <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+            <div className="box">
+                <h3>Informacije o troškovima</h3>
+                <p>Ukupni prihvatljivi troškovi (EUR): 209.364,52</p>
+                <p>Ukupna bespovratna sredstva (EUR): 177.959,84 (EU sufinanciranje)</p>
             </div>
         </div>
         <EUnoteProject />
